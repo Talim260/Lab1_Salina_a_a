36cf776b8cd68634ff900652461dfdb9547e0539
Talim260 <72585126+Talim260@users.noreply.github.com>
2020-11-04 20:00:41 +0300

    Add files via upload

---
+++ Lab2_java/src/Lab2/Author.java
@@ -0,0 +1,40 @@
+package Lab2;
+import java.lang.*;
+
+public class Author {
+    private String Name;
+
+    private String email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        Name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+
+    //Геттеры и Сеттеры
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    //toString()
+    @Override
+    public String toString() {
+        return (Name + " (" + gender + ") at " + email);
+
+    }
+}
---
+++ Lab2_java/src/Lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package Lab2;
+
+public class TestAuthor {public static void main(String[] args) {
+    Author a1 = new Author("Salina Alexandra Andreevna", "salina_a_a@mail.ru", 'F');
+    System.out.println( a1);
+}
+}
+
